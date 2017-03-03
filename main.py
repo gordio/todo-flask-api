45f1c5210b5d31215ab6bac259845c218282b4b2
Oleg Gordienko <admin@gordio.pp.ua>
2017-03-03 04:54:09 +0200

    todo: Return single task info

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-from flask import Flask
+from flask import Flask, jsonity, make_response
 
 
 app = Flask(__name__)
@@ -17,9 +17,22 @@ tasks = [{
 }]
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
 @app.route('/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+
+@app.route('/api/v1.0/task/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task})
 
 
 if __name__ == '__main__':
