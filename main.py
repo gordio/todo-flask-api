c92d4c97600ab4f0a0c931aed0a40e26267fc95f
Oleg Gordienko <admin@gordio.pp.ua>
2017-03-03 03:37:16 +0200

    todo: Return full list of tasks

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Hello here"
+tasks = [{
+    'id': 1,
+    'title': "Task 1",
+    'done': False
+}, {
+    'id': 2,
+    'title': "Task 2",
+    'done': False
+}]
+
+
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 
 if __name__ == '__main__':
